5cfa2481f867cbe2c91cc06cf3164daa8801e160
brzyang <yangtzerong@hotmail.com>
2020-06-08 01:20:20 +0800

    https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/

---
+++ src/main/java/com/leetcode/cn/array/MissNumber53.java
@@ -0,0 +1,23 @@
+package com.leetcode.cn.array;
+
+public class MissNumber53 {
+
+
+    public static class Solution {
+        public int missingNumber(int[] nums) {
+            int n = nums.length;
+            int sum = n*(n+1)/2;
+            int missSum = 0;
+            for (int val : nums){
+                missSum += val;
+            }
+
+            return sum - missSum;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().missingNumber(new int[]{0,1,2,3,4,5,6,7,9}));
+        System.out.println(new Solution().missingNumber(new int[]{0,1,3}));
+    }
+}
