2f4f772aa6ef08e1a42903f407a31d7b446e311c
brzyang <yangtzerong@hotmail.com>
2020-10-03 00:49:28 +0800

    HasCycleLinkedList2Ptr DetectCycleLinkedList2Ptr

---
+++ src/main/java/com/leetcode/cn/list/DetectCycleLinkedList2Ptr.java
@@ -0,0 +1,42 @@
+package com.leetcode.cn.list;
+
+import com.alphatok.domain.ListNode;
+
+public class DetectCycleLinkedList2Ptr {
+    public static class Solution {
+        public ListNode detectCycle(ListNode head) {
+            // x = (n-2m-1)L +z
+            if (head == null || head.next == null) {
+                return null;
+            }
+
+            ListNode interNode = findInterNode(head);
+            if (interNode == null) {
+                return null;
+            }
+
+            ListNode fast = interNode;
+            ListNode slow = head;
+            while (fast != slow){
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            return fast;
+        }
+
+        private ListNode findInterNode(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null){
+                fast = fast.next.next;
+                slow = slow.next;
+                if (slow == fast){
+                    return slow;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/test/java/com/leetcode/cn/list/HasCycleLinkedList2PtrTest.java
@@ -0,0 +1,61 @@
+package com.leetcode.cn.list;
+
+import com.alphatok.domain.ListNode;
+import com.alphatok.domain.ListNodeBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HasCycleLinkedList2PtrTest {
+
+    @Test
+    public void testHasCycle() {
+        HasCycleLinkedList2Ptr.Solution solution =  new HasCycleLinkedList2Ptr.Solution();
+        Assert.assertFalse(solution.hasCycle(null));
+        Assert.assertFalse(solution.hasCycle(new ListNode(-1)));
+
+        Assert.assertFalse(solution.hasCycle(new ListNodeBuilder(new int[]{1,1,1,1,1}).getHead()));
+
+
+        {
+            ListNode head = new ListNode(-1);
+            head.next = head;
+            Assert.assertTrue(solution.hasCycle(head));
+        }
+
+        {
+            ListNode head = new ListNode(-1);
+            ListNode p1 = new ListNode(1);
+            ListNode p2 = new ListNode(2);
+            head.next = p1;
+            p1.next = p2;
+            p2.next = head;
+            Assert.assertTrue(solution.hasCycle(head));
+        }
+
+        {
+            ListNode head = new ListNode(-1);
+            ListNode p1 = new ListNode(1);
+            ListNode p2 = new ListNode(2);
+            ListNode p3 = new ListNode(3);
+            head.next = p1;
+            p1.next = p2;
+            p2.next = p3;
+            p3.next = head;
+            Assert.assertTrue(solution.hasCycle(head));
+        }
+
+        {
+            ListNode head = new ListNode(-1);
+            ListNode p1 = new ListNode(1);
+            ListNode p2 = new ListNode(2);
+            ListNode p3 = new ListNode(3);
+            ListNode p4 = new ListNode(4);
+            head.next = p1;
+            p1.next = p2;
+            p2.next = p3;
+            p3.next = p4;
+            p4.next = p2;
+            Assert.assertTrue(solution.hasCycle(head));
+        }
+    }
+}
