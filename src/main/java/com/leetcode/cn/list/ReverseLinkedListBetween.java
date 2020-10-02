29db0092fff51921ae8454f49aa8cc6effd008af
brzyang <yangtzerong@hotmail.com>
2020-10-02 22:34:15 +0800

    ReverseLinkedListBetween

--- src/main/java/com/leetcode/cn/list/ReverseLinkedListBetween.java
+++ src/main/java/com/leetcode/cn/list/ReverseLinkedListBetween.java
@@ -3,7 +3,7 @@ package com.leetcode.cn.list;
 import com.alphatok.domain.ListNode;
 
 public class ReverseLinkedListBetween {
-    public class Solution {
+    public static class Solution {
         /**
          *
          * @param head ListNode类
@@ -11,9 +11,43 @@ public class ReverseLinkedListBetween {
          * @param n int整型
          * @return ListNode类
          */
-        public ListNode reverseBetween (ListNode head, int m, int n) {
-            // write code here
-            return null;
+        public ListNode reverseBetween(ListNode head, int m, int n) {
+            if(m == n || head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+
+            ListNode pre = dummy;
+            ListNode cur = head;
+            // scan to m
+            int curIndex = 1;
+            for (; curIndex < m; curIndex++) {
+                pre = pre.next;
+                cur = cur.next;
+            }
+
+            // cur move to m+1
+            curIndex++;
+            ListNode hookNode = pre;
+            pre = pre.next;
+            cur = cur.next;
+
+            while (curIndex <= n && cur != null){
+                ListNode next = cur.next;
+
+                // cut cur from list
+                pre.next = cur.next;
+                // insert cur after hookNode
+                cur.next = hookNode.next;
+                hookNode.next = cur;
+
+                cur = next;
+                curIndex++;
+            }
+
+            return dummy.next;
         }
     }
 }
